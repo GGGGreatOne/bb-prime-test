7da299732715a7ca7c17cb68394ab2c05607a676
GGGGreatOne <283548875@qq.com>
2025-05-19 19:06:31 +0800

    fix: fix eslint

--- src/layout/localUserInfoProvider.tsx
+++ src/layout/localUserInfoProvider.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react'
 import { LOCAL_USER_INFO, setLocalUserInfo } from '@/hooks/useLocalUserInfo'
 
-
 export const LocalUserInfoProvider = ({ children }: { children: React.ReactNode }) => {
 	useEffect(() => {
 		const userinfo = JSON.parse(localStorage.getItem(LOCAL_USER_INFO) ?? '{}')
